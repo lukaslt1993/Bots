5046e3768b760257c529b7c1c63182198d28cf4f
warrior55 <warrior55@1f8232c4-8d52-48f9-84fd-8e26e50a0a51>
2019-08-05 09:14:10 +0000

    Fix world hop and regex for crafter

--- src/com/runemate/warrior55/tanner/common/Constants.java
+++ src/com/runemate/warrior55/tanner/common/Constants.java
@@ -5,5 +5,6 @@ import java.util.regex.Pattern;
 
 public class Constants {
     
-    public static final Pattern PATTERN = Pattern.compile("^.* dragonhide$");
+    //public static final Pattern PATTERN = Pattern.compile("^.* dragonhide$");
+    public static final Pattern PATTERN = Pattern.compile("^.*hide$");
 }
--- src/com/runemate/warrior55/tanner/main/PortableCrafter.java
+++ src/com/runemate/warrior55/tanner/main/PortableCrafter.java
@@ -3,16 +3,43 @@ package com.runemate.warrior55.tanner.main;
 import com.runemate.game.api.script.framework.tree.TreeBot;
 import com.runemate.game.api.script.framework.tree.TreeTask;
 import com.runemate.warrior55.tanner.branches.RootBranch;
+import java.util.Optional;
+import javafx.application.Platform;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Alert.AlertType;
+import javafx.scene.control.ButtonType;
 
 public class PortableCrafter extends TreeBot {
+
+    private Boolean isMember = null;
 
     @Override
     public void onStart(String... args) {
         setLoopDelay(25, 50);
+        if (isMember == null) {
+            Platform.runLater(() -> {
+                Alert alert = new Alert(AlertType.CONFIRMATION);
+                alert.setTitle("Dialog");
+                alert.setHeaderText("Are you a member");
+
+                ButtonType yes = new ButtonType("Yes");
+                ButtonType no = new ButtonType("No");
+
+                alert.getButtonTypes().setAll(yes, no);
+
+                Optional<ButtonType> result = alert.showAndWait();
+                isMember = result.get() == yes;
+            });
+        }
     }
 
     @Override
     public TreeTask createRootTask() {
         return new RootBranch();
     }
+
+    public boolean isMember() {
+        return isMember;
+    }
+
 }
