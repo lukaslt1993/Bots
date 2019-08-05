86ded6e7a006a4f5d988d91cc75684978245414c
warrior55 <warrior55@1f8232c4-8d52-48f9-84fd-8e26e50a0a51>
2019-08-05 09:20:49 +0000

    Remove comments for crafter

--- RedSpidersEggs/src/com/runemate/warrior55/tanner/common/Constants.java
+++ RedSpidersEggs/src/com/runemate/warrior55/tanner/common/Constants.java
@@ -4,7 +4,5 @@ package com.runemate.warrior55.tanner.common;
 import java.util.regex.Pattern;
 
 public class Constants {
-    
-    //public static final Pattern PATTERN = Pattern.compile("^.* dragonhide$");
     public static final Pattern PATTERN = Pattern.compile("^.*hide$");
 }
